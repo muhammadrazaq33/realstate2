ab5644b629007aa3d0e7980a0550f4f6bb5507ae
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-01-24 18:49:20 +0500

    Error resolved

--- app/GlobalComponents.js/Links.js
+++ app/GlobalComponents.js/Links.js
@@ -3,7 +3,7 @@ import React from "react";
 const Links = ({ links }) => {
   return (
     <>
-      {links.map((index, link) => {
+      {links.map((link, index) => {
         return (
           <div key={index}>
             <p className="text-[#0E0E0E] text-[1rem] hover:text-[#267777] cursor-pointer">
